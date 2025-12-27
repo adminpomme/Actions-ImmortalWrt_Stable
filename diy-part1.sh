8a55a6444bc25f68004ea09c7634ddebe196bbe9
adminpomme <admin.pomme@gmail.com>
2025-12-27 15:41:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+添加feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
